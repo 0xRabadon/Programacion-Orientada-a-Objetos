5aad34a65a11d6f5fb62923365867382db851b4c
0xRabadon <lovallenunez@gmail.com>
2026-03-23 18:45:33 -0300

    estudiando clases

--- tonteras_para_practicar/src/tonteras_para_practicar_clases/AutoTest.java
+++ tonteras_para_practicar/src/tonteras_para_practicar_clases/AutoTest.java
@@ -3,8 +3,23 @@ package tonteras_para_practicar_clases;
 public class AutoTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Auto miAuto = new Auto(); // declaramos nuestro primer auto un kia azul
+		
+		miAuto.kilometraje = 0.0;
+		miAuto.patente = "ABC123";
+		miAuto.color = "azul";
+		miAuto.marca = "kia";
+		
+		Auto otroAuto = new Auto(); // declaramos otro auto un toyota blanco y negro
+		
+		otroAuto.kilometraje = 313123.231313131;
+		otroAuto.patente = "AE86";
+		otroAuto.marca = "Toyota";
+		otroAuto.color = "panda";
+		
+		miAuto.selMarca("nissan");
+		String marca1;
+		
 	}
 
 }
